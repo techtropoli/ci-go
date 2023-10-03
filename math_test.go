b04b6707cc5a0c2d1b2cea79aa8635d124b0e145
Gabriele Almeida <naogabe@gmail.com>
2023-10-03 15:22:28 -0300

    feat: :sparkles: add minus function

--- math_test.go
+++ math_test.go
@@ -9,3 +9,11 @@ func TestSoma(t *testing.T) {
 		t.Errorf("Resultado da soma é invalido: Resultado %d. Esperado: %d.", total, 30)
 	}
 }
+
+func TestSubtracao(t *testing.T) {
+	total := subtracao(15, 15)
+
+	if total != 0 {
+		t.Errorf("Resultado da subtração é invalido: Resultado %d. Esperado: %d.", total, 0)
+	}
+}
