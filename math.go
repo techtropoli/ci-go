2aae14b1911d39f6dd5da9af536f7cb78b1c6a76
Gabriele Almeida <naogabe@gmail.com>
2023-10-03 13:48:49 -0300

    feat: unpublish plus function
    other packages wont be able to use this function

--- math.go
+++ math.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Soma(10, 10))
+	fmt.Println(soma(10, 10))
 }
 
-func Soma(a int, b int) int {
+func soma(a int, b int) int {
 	return a + b
 }
